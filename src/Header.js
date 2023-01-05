0bbd070d7289f45b542c1c3f092b5b6cb6f4a097
Nikhil Vijayan <nikhil321vijayan@gmail.com>
2023-01-05 18:01:58 -0500

    Added a Header component
    * Added Header component with the linkedin icon, a search icon and search bar.
    * Added a HeaderOptions component that contains an icon and a option text.
    * Added Home, My Network, Jobs, Messaging and NotificationOptions on the header.
    * Added styling to the Header and HeaderOption components.
    * Made sure the header sticks to the top with :
        position: sticky;
        top: 0;

---
+++ src/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import { BusinessCenter, Chat, Notifications } from "@mui/icons-material";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/3536/3536505.png"
+          alt=""
+        />
+
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenter} title="Jobs" />
+        <HeaderOption Icon={Chat} title="Messaging" />
+        <HeaderOption Icon={Notifications} title="Notifications" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
