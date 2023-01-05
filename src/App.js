5f8df00e1d5874442df43b0ca9f38e5e86a4c9a7
Nikhil Vijayan <nikhil321vijayan@gmail.com>
2023-01-05 14:30:26 -0500

    Fleshing out the app component skeleton

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ function App() {
   return (
     <div className="app">
       <h1>LinkedIn clone</h1>
+
+      {/* Header */}
+      {/* App Body */}
+      {/* Sidebar */}
+      {/* Feed */}
+      {/* Widgets */}
     </div>
   );
 }
