f5d27cbdcbcadaeaece9c60a663f227a6000bb12
Nikhil Vijayan <nikhil321vijayan@gmail.com>
2023-01-07 20:26:47 -0500

    Added the Sidebar component
    * Added Avatar, cover image, Views, Recent section etc in the Sidebar component

---
+++ src/Sidebar.js
@@ -0,0 +1,47 @@
+import { Avatar } from "@mui/material";
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+  const recentItem = (topic) => (
+    <div className="sidebar__recentItem">
+      <span className="sidebar__hash">#</span>
+      <p>{topic}</p>
+    </div>
+  );
+
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <img
+          src="https://images.unsplash.com/photo-1553152531-b98a2fc8d3bf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80"
+          alt=""
+        />
+        <Avatar className="sidebar__avatar" />
+        <h2>Nikhil Vijayan</h2>
+        <h4>nikhil321vijayan@gmail.com</h4>
+      </div>
+
+      <div className="sidebar__stats">
+        <div className="sidebar__stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber">1,435</p>
+        </div>
+        <div className="sidebar__stat">
+          <p>Views on post</p>
+          <p className="sidebar__statNumber">994</p>
+        </div>
+      </div>
+
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+        {recentItem("reactjs")}
+        {recentItem("programming")}
+        {recentItem("frontenddev")}
+        {recentItem("helloworld")}
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
